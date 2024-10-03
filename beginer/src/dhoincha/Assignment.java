2a443c793384fa6b53a496b0038324afd634f491
naimurrahman144 <naimur.ru.cse@gmail.com>
2024-10-03 20:42:56 +0600

    2nd commit

---
+++ beginer/src/dhoincha/Assignment.java
@@ -0,0 +1,10 @@
+package dhoincha;
+
+public class Assignment {
+    public static void main(String[] args){
+        int sum = 4+2 ;
+        System.out.print(sum);
+        System.out.println();
+        System.out.print(sum);
+    }
+}
